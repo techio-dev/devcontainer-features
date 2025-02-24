56dcbc0b135c826436894e352e3b6e4cd37dee3e
Your Name <you@example.com>
2025-02-24 16:33:28 +0700

    feat(install): add Debian dependency installation and checks
    Implement a function to install Debian dependencies and ensure the script 
    is run as root. Add support for identifying the Linux distribution and 
    installing packages accordingly. This enhances compatibility and 
    usability for users on Debian-based systems.

--- src/neovim/install.sh
+++ src/neovim/install.sh
@@ -1,7 +1,40 @@
 #!/bin/sh
 set -e
 
+install_debian_dependencies() {
+  apt-get update -y
+  apt-get -y install curl
+  apt-get -y clean
+  rm -rf /var/lib/apt/lists/*
+}
+
 echo "Activating feature 'neovim'"
+
+if [ "$(id -u)" -ne 0 ]; then
+  echo -e 'Script must be run as root. Use sudo, su, or add "USER root" to your Dockerfile before running this script.'
+  exit 1
+fi
+# Bring in ID, ID_LIKE, VERSION_ID, VERSION_CODENAME
+. /etc/os-release
+# Get an adjusted ID independent of distro variants
+if [ "${ID}" = "debian" ] || [ "${ID_LIKE}" = "debian" ]; then
+  ADJUSTED_ID="debian"
+  # other distros to be implemented
+  # elif [[ "${ID}" = "rhel" || "${ID}" = "fedora" || "${ID}" = "mariner" || "${ID_LIKE}" = *"rhel"* || "${ID_LIKE}" = *"fedora"* || "${ID_LIKE}" = *"mariner"* ]]; then
+  # todo
+  # elif [ "${ID}" = "alpine" ]; then
+  # todo
+else
+  echo "Linux distro ${ID} not supported."
+  exit 1
+fi
+
+# Install packages for appropriate OS
+case "${ADJUSTED_ID}" in
+"debian")
+  install_debian_dependencies
+  ;;
+esac
 
 VERSION=${VERSION:-stable}
 
